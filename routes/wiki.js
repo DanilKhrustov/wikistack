a20287acc760762c76550e052355f22023ac9f8b
Danil Khrustov <khrustovd@gmail.com>
2019-03-07 15:17:31 -0600

    work

--- routes/wiki.js
+++ routes/wiki.js
@@ -2,6 +2,7 @@ const model = require('../models');
 const views = require('../views');
 const viewsLayout = require('../views/layout');
 const express = require('express');
+//const { Page } = require("../models");
 
 const router = express.Router();
 
@@ -13,8 +14,21 @@ router.get('/add', async (req, res, next) => {
   res.send(views.addPage())
 });
 
-router.post('/', async (req, res, next) => {
-  res.send('postwiki')
+router.post('/add', async (req, res, next) => {
+  const page = new model.Page({
+    title: req.body.title,
+    content: req.body.content,
+    status: 'open',
+
+  });
+
+  // make sure we only redirect *after* our save is complete!
+  // note: `.save` returns a promise.
+  try {
+    await page.save();
+    res.redirect('/');
+  } catch (error) { next(error) }
+  //res.redirect('/');
 });
 
 
