92768c556b984055793bf6df3f5b941ff4cea4b2
Zubair Anwar <zubairanwar@icloud.com>
2019-03-07 12:05:02 -0600

    work

--- app.js
+++ app.js
@@ -2,16 +2,14 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const model = require('./models/index');
-
-const PORT = 3300;
-
 const layout = require('./views/layout');
-
-
+const routerWiki = require('./routes/wiki');
+//const routerUser = require('./routes/user');
 
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/public"));
+app.use('/wiki', routerWiki);
 
 app.get('/', (req, res) => {
     res.send(layout('test'))
@@ -22,6 +20,7 @@ model.db.authenticate().
         console.log('connected to the database');
     })
 
+const PORT = 3300;
 const init = async () => {
     await model.db.sync()
     app.listen(PORT, () => {
