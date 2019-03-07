8990c06359ce37ff84c214f6acdecc94a95544a8
Danil Khrustov <khrustovd@gmail.com>
2019-03-07 11:03:32 -0600

    work

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const app = express();
 const morgan = require('morgan');
 const PORT = 3300;
 
+const layout = require('./views/layout');
+
+
+
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/public"));
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.send(layout('test'))
 })
 
 app.listen(PORT, () => {
