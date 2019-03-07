ea20b5e205974c7f250eeb29d0176ff504fe09c6
Danil Khrustov <khrustovd@gmail.com>
2019-03-07 10:57:33 -0600

    changes

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 const PORT = 3300;
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + "/public"));
 
 app.get('/', (req, res) => {
     res.send('Hello World')
